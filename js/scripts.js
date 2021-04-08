9c50655aa1a5016f5cbce826e475d5dae8d69b2a
juma-moringa <allan.juma@student.moringaschool.com>
2021-04-09 01:33:53 +0300

    introduced conditions on the js

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,17 @@ function results (){
     let gender = print();
     outputDay = date.getDay();
     let akanName;
+
+    if ( year < 1900 || year > 2050){
+        alert("Opps something wasn't right. Please try again.");
+        return false;
+    }
+    if ( month < 1 || month > 12 ){
+        alert("Unexpected error has occured .Please input the correct month.");
+        return false;
+    }
+    
+    if (day < 1 || day > 31){
+        alert("Opps something wasn't right. Please input the correct day again.")
+    }
 }
