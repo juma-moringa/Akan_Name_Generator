f90210fa8ee70a666c3c04dc825e9602b7a275e8
juma-moringa <allan.juma@student.moringaschool.com>
2021-04-09 01:14:03 +0300

    add initial js arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
+let dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let Female = ["Akosua", "Adowoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+let male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
+function results (){
+    let year = parseInt(document.getElementById("year").value);
+    let month = parseInt(document.getElementById("month").value);
+    let day = parseInt(document.getElementById("day").value);
+    let date = outputDate(year + "-" + month + "-" + day);
+    let gender = print();
+    outputDay = date.getDay();
+    let akanName;
+}
