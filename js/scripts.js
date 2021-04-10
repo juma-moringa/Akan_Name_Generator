75fd8ebf68f37923ff6451d6fb03385cff03e17e
juma-moringa <allan.juma@student.moringaschool.com>
2021-04-10 18:34:20 +0300

    updates done on js

--- js/scripts.js
+++ js/scripts.js
@@ -2,53 +2,51 @@ let dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fri
 let Female = ["Akosua", "Adowoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 let Male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-function results (){
+function results() {
     let year = parseInt(document.getElementById("year").value);
     let month = parseInt(document.getElementById("month").value);
     let day = parseInt(document.getElementById("day").value);
     let date = outputDate(year + "/" + month + "/" + day);
     let gender = print();
-    outputDay = date.getDay();
+    let outputDay = date.getDay();
     let akanName;
 
-    if ( year < 1900 || year > 2050){
+    if (year < 1900 || year > 2050) {
         alert("Opps something wasn't right. Please try again.");
         return false;
 
     }
-    if ( month < 1 || month > 12 ){
+    if (month < 1 || month > 12) {
         alert("Unexpected error has occured .Please input the correct month.");
         return false;
     }
-    
-    if (day < 1 || day > 31){
+
+    if (day < 1 || day > 31) {
         alert("Opps something wasn't right. Please input the correct day again.");
         return false;
 
     }
-    if(gender === "male"){
+    if (gender === "male") {
         akanName = Male[outputDay];
 
-    }
-    else {
+    } else {
         akanName = Female[outputDay];
     }
 
-    alert("Your Akan name is  "+akanName+ " and you were born on "+ dayOfTheWeek[outputDay]);
+    alert("You were born on "+ dayOfTheWeek[outputDay] + " and your akan name is" + akanName);
 }
 
-function print(){
+function print() {
     let gender = document.getElementByName("gender");
-for (i = 0; i < gender.length; i ++) {
-    if(gender[i] .checked) {
-        return( gender[i] .value)
+    for (i = 0; i < gender.length; i++) {
+        if (gender[i].checked) {
+            return (gender[i].value)
+        }
     }
 }
-}
+
 function reset() {
     document.getElementById("buttons").reset();
-  } 
+}
 
-
-
-  
+const result_p = document.getElementaryById("result")
